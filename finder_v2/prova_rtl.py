02d75babaf56db91d00a04810f54ad13a226352a
unknown <arnaucosta95@gmail.com>
2020-10-02 13:13:22 +0200

    add prova al rtl per saber quines dades dona

--- finder_v2/prova_rtl.py
+++ finder_v2/prova_rtl.py
@@ -11,6 +11,7 @@ from rtlsdr import RtlSdr
 
 
 NUM_BYTES = 1024
+maxim = -999
 
 
 sdr                 =  RtlSdr()
@@ -32,9 +33,21 @@ def get_signal_power():
     return max_signal
 
 
-
+while True:
 #Llegeixo dades del RTLSDR. Divideixo el NFFT en 4 per tenir menys data i que operi mes rapid. Revisar aizo del NUM_BYTES
-x = sdr.read_samples(NUM_BYTES)
-print (len(x))
-power, _ = mlab.psd(x, NFFT=NUM_BYTES//4, pad_to=NUM_BYTES, Fs=sdr.sample_rate/1e6)
-print(len(power))
+	x = sdr.read_samples(NUM_BYTES)
+	print (len(x))
+	power, _ = mlab.psd(x, NFFT=NUM_BYTES//4, pad_to=NUM_BYTES, Fs=sdr.sample_rate/1e6)
+	print(len(power))
+
+	maxim = get_signal_power()
+
+
+
+	#Opening and Closing a file "MyFile.txt" 
+	# for object name file1. 
+	file1 = open("MyFile.txt","a") 
+	file1.write(f"{maxim} \n") 
+	file1.close() 
+
+	sleep(0.2)
