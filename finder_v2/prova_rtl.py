99afc8cce2a90b53e607ae14c5f3b24cd5614841
unknown <arnaucosta95@gmail.com>
2020-10-02 10:36:32 +0200

    add prova rtl

---
+++ finder_v2/prova_rtl.py
@@ -0,0 +1,40 @@
+#Llibreries generals
+import threading
+import os
+import serial
+import math
+from matplotlib import mlab as mlab
+from time import sleep
+
+#Llibreries RTLSDR
+from rtlsdr import RtlSdr
+
+
+NUM_BYTES = 1024
+
+
+sdr                 =  RtlSdr()
+sdr.sample_rate     =  1e6  # Hz
+sdr.center_freq     =  27.035e6  # Hz
+sdr.freq_correction =  60   # PPM
+sdr.gain            =  'auto'
+
+
+#Captura la maxima senyal del RTLSDR.
+def get_signal_power():
+	max_signal=-99
+	middle = int(len(power) / 2)
+	middle_data = power[middle-50:middle+50]
+
+	for dat in power:
+    	if dat > max_signal:
+        	max_signal = dat
+    return max_signal
+
+
+
+#Llegeixo dades del RTLSDR. Divideixo el NFFT en 4 per tenir menys data i que operi mes rapid. Revisar aizo del NUM_BYTES
+x = sdr.read_samples(NUM_BYTES)
+print (len(x))
+power, _ = mlab.psd(x, NFFT=NUM_BYTES//4, pad_to=NUM_BYTES, Fs=sdr.sample_rate/1e6)
+print(len(power))
